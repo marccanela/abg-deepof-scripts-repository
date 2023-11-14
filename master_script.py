cf42c6193ae1b9409c5fca2d52dd4f0ed75ef8b1
Canela Grimau <mcanela@researchmar.net>
2023-11-14 11:09:45 +0100

    updated up to supervised analysis

--- master_script.py
+++ master_script.py
@@ -12,17 +12,11 @@ import pandas as pd
 import deepof.data
 import numpy as np
 
-# NOTE: Run the corrected videos with the DLC files of the corrected videos
-
-#directory_output = '//FOLDER/becell/Lab Projects/ERCstG_HighMemory/Data/Marc/1) SOC/2023-09 - Young males/DeepOF/'
-#directory_dlc = '//FOLDER/becell/Lab Projects/ERCstG_HighMemory/Data/Marc/1) SOC/2023-09 - Young males/DeepOF/Data/dlc/'
-#directory_videos = '//FOLDER/becell/Lab Projects/ERCstG_HighMemory/Data/Marc/1) SOC/2023-09 - Young males/DeepOF/Data/corrected videos/'
-
 # =============================================================================
 # Creating a new DeepOF project
 # =============================================================================
 
-def creating_deepof_project(directory_output, directory_dlc, directory_videos, manual=False, scale=200):
+def creating_deepof_project(directory_output, directory_dlc, directory_videos, manual, scale):
     if manual == False:
         arena = 'polygonal-autodetect'
     elif manual == True:
@@ -47,20 +41,12 @@ def creating_deepof_project(directory_output, directory_dlc, directory_videos, m
     
     return my_deepof_project
 
-#my_deepof_project = creating_deepof_project(directory_output, directory_dlc, directory_videos)
-
-# OPTIONAL: Edit the arena size of some given videos after the project is already created
-#my_deepof_project.edit_arenas(videos=['20230728_Marc_ERC SOC S1_Males_box ef_05_01_1'])
-
 # =============================================================================
 # Uploading conditions to my DeepOF project
 # =============================================================================
 
-# Uploading the experimental conditions to the DeepOF project
-#my_deepof_project.load_exp_conditions(directory_output + "conditions.csv")
-
 # Check if everything is correct
-def check_conditions(directory_output, my_deepof_project, col_name='protocol'):
+def check_conditions(directory_output, my_deepof_project, col_name):
     conditions = np.unique(pd.read_csv(directory_output + "conditions.csv", index_col=0)[col_name].to_list())
     coords = my_deepof_project.get_coords()
     print("The dataset has {} videos:".format(len(coords)))
@@ -72,10 +58,6 @@ def check_conditions(directory_output, my_deepof_project, col_name='protocol'):
 # =============================================================================
 # Loading a previously generated project
 # =============================================================================
-
-#my_deepof_project = deepof.data.load_project(directory_output + "deepof_tutorial_project")
-
-#directory_path = '//FOLDER/becell/Lab Projects/ERCstG_HighMemory/Data/Marc/1) SOC/2023-09 - Young males/DeepOF/Data/pickles/'
 
 def update_supervised_annotation_with_immobility(supervised_annotation, directory_path):
     for filename in os.listdir(directory_path):
