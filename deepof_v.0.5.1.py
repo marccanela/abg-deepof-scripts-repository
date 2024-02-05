66bcc015b89b04964b098cf65d3cafe83b4b09c7
mcanela <mcanelag99@gmail.com>
2024-02-05 19:32:04 +0100

    update

--- deepof_v.0.5.1.py
+++ deepof_v.0.5.1.py
@@ -1,7 +1,7 @@
 """
-SINGLE ANIMAL
+SINGLE ANIMAL (5/2/2024)
 DeepOF v.0.5.1
-conda activate deepof2
+activate_conda env deepof2
 Analyze with DLC multia_bed_14_keyp-mcanela-2023-11-16
 """
 
@@ -14,19 +14,18 @@ import deepof.visuals
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Define directories
+directory_output = '/home/sie/Desktop/marc/brain_01a02/'
+directory_dlc = '/home/sie/Desktop/marc/brain_01a02/csv_corrected/'
+directory_videos = '/home/sie/Desktop/marc/brain_01a02/avi_corrected/'
+
 # Convert CSV-multi-animal into CSV-single-animal
-directory_csv = '/home/sie/Desktop/marc/data_controls_dlc/csv_converted'
-for file in os.listdir(directory_csv):
+for file in os.listdir(directory_dlc):
     if file.endswith('.csv'):
-        file_path = os.path.join(directory_csv, file)
+        file_path = os.path.join(directory_dlc, file)
         df = pd.read_csv(file_path, header=None)
         df_modified = df.drop(1)
         df_modified.to_csv(file_path[:-7] + '.csv', index=False, header=False)
-
-# Define directories
-directory_output = '/home/sie/Desktop/marc/data_controls_dlc'
-directory_dlc = '/home/sie/Desktop/marc/data_controls_dlc/csv_converted'
-directory_videos = '/home/sie/Desktop/marc/data_controls_dlc/avi'
 
 # Prepare the project
 my_deepof_project_raw = deepof.data.Project(
@@ -35,7 +34,7 @@ my_deepof_project_raw = deepof.data.Project(
                 table_path=os.path.join(directory_dlc),
                 project_name="deepof_tutorial_project",
                 arena="polygonal-manual",
-                # animal_ids=['A'],
+                # animal_ids=['mouse'],
                 table_format=".csv",
                 video_format=".avi",
                 bodypart_graph='deepof_14',
@@ -50,17 +49,17 @@ my_deepof_project = my_deepof_project_raw.create(force=True)
 
 # Edit wrong arenas
 my_deepof_project.edit_arenas(
-    videos=['20240119_Marc_ERC SOC light_Males_box de_06_01_1'],
+    videos=['20240202_Marc_ERC SOC tone_Males_box ab_240003812_02_01_1'],
     arena_type="polygonal-manual",
 )
 
 # Load conditions
-my_deepof_project.load_exp_conditions('/home/sie/Desktop/marc/data_controls_dlc/conditions.csv')
+my_deepof_project.load_exp_conditions(directory_output + 'conditions.csv')
 
 # Check conditions
 coords = my_deepof_project.get_coords()
 print("The original dataset has {} videos".format(len(coords)))
-coords = coords.filter_condition({"protocol": "s2"})
+coords = coords.filter_condition({"protocol": "paired"})
 print("The filtered dataset has only {} videos".format(len(coords)))
 
 # Load a previously saved project
